20fb6d90299f03e3390ea22231a80512a4ac8f51
Filip Konieczny <filkonieczny@gmail.com>
2018-06-01 15:33:54 +0200

    move protobuf script to js folder

--- js/generate_protobuf.sh
+++ js/generate_protobuf.sh
@@ -1,11 +1,13 @@
 # Path to this plugin
-PROTOC_GEN_TS_PATH="./js/node_modules/.bin/protoc-gen-ts"
+PROTOC_GEN_TS_PATH="./node_modules/.bin/protoc-gen-ts"
 
 # Directory to write generated code to (.js and .d.ts files) 
-OUT_DIR="./js/src/protobuf"
+OUT_DIR="./src/protobuf"
 
-./protoc \
+../protoc \
     --plugin="protoc-gen-ts=${PROTOC_GEN_TS_PATH}" \
     --js_out="import_style=commonjs,binary:${OUT_DIR}" \
     --ts_out="${OUT_DIR}" \
+    --proto_path=".." \
     Message.proto
+    
